5d4a5f347cc151836cac8938666e16af758155da
Jean Luc Abayo <luc.bayo@gmail.com>
2017-07-18 11:25:41 +0200

    created studentinfos controller

---
+++ app/controllers/student_infos_controller.rb
@@ -0,0 +1,33 @@
+class StudentInfosController < ApplicationController
+  def index
+    
+  end
+
+  def show
+    
+  end
+
+  def new
+    @student_infos = StudentInfo.new
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+  private
+  def find_student_infos
+    
+  end
+  def student_infos_params
+    
+  end
+end
+
