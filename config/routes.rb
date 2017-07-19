58b37dcd2261fd390516fd6dab85d4bcb773c341
plantejosue <plantejosh1@gmail.com>
2017-07-19 11:52:33 +0200

    application step 1 & 2 designs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :student_infos
+
+  get "/application1" => "designs#application1"
+  get "/application2" => "designs#application2"
 end
